02ef78284b71066458612d82d509e8c6d075ea36
Michiel de Jong <michiel@unhosted.org>
2011-04-05 14:44:07 +0200

    compliance with http://unhosted.org/spec/DAV/2011-04

--- demoRedlibre/webfinger/index.php
+++ demoRedlibre/webfinger/index.php
@@ -2,7 +2,9 @@
 if(isset($_GET['q'])) {
 	$r = array(
 		"subject" => "acct:demo@demo.redlibre.org",
-		"links" => array('http://unhosted.org/spec/DAV/' => array('http://demo.redlibre.org/'))
+		"links" => array(
+			'http://unhosted.org/spec/DAV/2011-04/' => array('http://demo.redlibre.org/')
+		)
 	);
 	echo json_encode($r);
 }
