106b3982f65088859c39fea4063e1ea33dd48baa
Michiel de Jong <michiel@unhosted.org>
2011-04-09 23:38:43 +0200

    switched from jrd to xrd and put domain before user in WebDAV url

--- demoRedlibre/webfinger/index.php
+++ demoRedlibre/webfinger/index.php
@@ -1,10 +1,14 @@
 <?php
 if(isset($_GET['q'])) {
-	$r = array(
-		"subject" => "acct:demo@demo.redlibre.org",
-		"links" => array(
-			'http://unhosted.org/spec/DAV/2011-04/' => array('http://demo.redlibre.org/')
-		)
-	);
-	echo json_encode($r);
+	header('Content-Type: application/xml+xrd');
+	//header('Content-Type: text/xml');
+
+	echo "<?xml version='1.0' encoding='UTF-8'?>\n";
+	echo "<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'\n"; 
+	echo "      xmlns:hm='http://host-meta.net/xrd/1.0'>\n";
+	echo "  <hm:Host xmlns='http://host-meta.net/xrd/1.0'>demo.redlibre.org</hm:Host>\n";
+	echo "  <Link rel='http://unhosted.org/spec/dav/0.1'\n";
+	echo "      href='http://demo.redlibre.org/'>\n";
+	echo "  </Link>\n";
+	echo "</XRD>\n";
 }
